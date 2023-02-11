270a77e73ec2db92dc95419730febdbcb73b34f4
Michel R <michelfr95@gmail.com>
2023-02-11 14:06:29 +0100

    feat: add support for environment variables in config service
    This commit adds support for environment variables in the config service. This allows the user to override the values in the config file with the values from the environment variables. This can be useful for setting different configurations for different environments, such as development, testing, and production.

--- src/services/ConfigService.ts
+++ src/services/ConfigService.ts
@@ -4,10 +4,15 @@ import fs from "fs";
 export default class ConfigService {
   public static getConfig(): any {
     try {
-      return yaml.parse(fs.readFileSync("config.yaml", "utf8"));
+      const config = yaml.parse(fs.readFileSync("config.yaml", "utf8"));
+      for (const key of Object.keys(config)) {
+        if (process.env[key]) {
+          config[key] = process.env[key];
+        }
+      }
+      return config;
     } catch (e) {
       console.log(e);
     }
   }
 }
-
