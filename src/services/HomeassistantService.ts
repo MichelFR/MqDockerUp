9e07601d5befc14c7007f1759f3f085079c218d9
Michel R <michelfr95@gmail.com>
2023-02-11 15:19:48 +0100

    Update HomeassistantService.ts

--- src/services/HomeassistantService.ts
+++ src/services/HomeassistantService.ts
@@ -1,102 +1,131 @@
-import mqtt from "mqtt";
-import DockerService from "./DockerService";
-import ConfigService from "./ConfigService";
+import { config } from "../config";
+import { client } from "../index";
+import { DockerService } from "./DockerService";
+import { TimeService } from "./TimeService";
+import Docker from "dockerode";
 
-const config = ConfigService.getConfig();
-
-export default class HomeassistantService {
-  static createEntities(client: mqtt.MqttClient): void {
-    DockerService.listContainers().then((containers) => {
-      for (const container of containers) {
+export class HomeassistantService {
+  public static createEntities(client: any) {
+    DockerService.getContainers().then((containers) => {
+      containers.forEach((container) => {
         const image = container.Config.Image;
         const imageInfo = DockerService.getImageInfo(image);
-        const currentTags = imageInfo.RepoTags.map((tag) => tag.split(":")[1]);
-
-        for (const currentTag of currentTags) {
+        const currentTags = imageInfo.RepoTags.map((tag: string) => tag.split(":")[1]);
+        currentTags.forEach((currentTag) => {
           const entity = {
-            name: `${container.Names[0].substring(1)}-${currentTag}`,
+            name: `${container.Name.substring(1)}-${currentTag}`,
             unique_id: `${container.Id}-${currentTag}`,
-            state_topic: `${config.mqtt.topic}/${container.Id}/${currentTag}/state`,
-            command_topic: `${config.mqtt.topic}/${container.Id}/${currentTag}/update`,
-            availability_topic: `${config.mqtt.topic}/availability`,
+            device: {
+              identifiers: [container.Id],
+              name: container.Name.substring(1),
+              model: image,
+              manufacturer: "Docker",
+            },
+            availability_topic: `${config.mqtt.topic}/${container.Id}/${currentTag}/availability`,
             payload_available: "online",
             payload_not_available: "offline",
-            icon: "mdi:docker",
+            state_topic: `${config.mqtt.topic}/${container.Id}/${currentTag}/state`,
+            json_attributes_topic: `${config.mqtt.topic}/${container.Id}/${currentTag}/attributes`,
+            command_topic: `${config.mqtt.topic}/${container.Id}/${currentTag}/update`,
           };
-
           client.publish(
             `homeassistant/sensor/${container.Id}/${currentTag}/config`,
             JSON.stringify(entity),
             { retain: true }
           );
-        }
-      }
+          client.publish(
+            `${config.mqtt.topic}/${container.Id}/${currentTag}/availability`,
+            "online",
+            { retain: true }
+          );
+        });
+      });
     });
   }
 
-  static createUpdate(
+  public static createUpdate(
     container: Docker.ContainerInspectInfo,
     currentTag: string,
     previousDigest: string,
     newDigest: string,
-    client: mqtt.MqttClient
-  ): void {
+    client: any
+  ) {
     const update = {
-      state: "available",
-      attributes: {
-        friendly_name: `${container.Names[0].substring(1)}-${currentTag}`,
+      name: `${container.Name.substring(1)}-${currentTag}-update`,
+      unique_id: `${container.Id}-${currentTag}-update`,
+      device: {
+        identifiers: [container.Id],
+        name: container.Name.substring(1),
+        model: container.Config.Image,
+        manufacturer: "Docker",
+      },
+      availability_topic: `${config.mqtt.topic}/${container.Id}/${currentTag}/availability`,
+      payload_available: "online",
+      payload_not_available: "offline",
+      state_topic: `${config.mqtt.topic}/${container.Id}/${currentTag}/update/state`,
+      json_attributes_topic: `${config.mqtt.topic}/${container.Id}/${currentTag}/update/attributes`,
+      command_topic: `${config.mqtt.topic}/${container.Id}/${currentTag}/update`,
+    };
+    client.publish(
+      `homeassistant/sensor/${container.Id}/${currentTag}-update/config`,
+      JSON.stringify(update),
+      { retain: true }
+    );
+    client.publish(
+      `${config.mqtt.topic}/${container.Id}/${currentTag}/update/state`,
+      "New version available",
+      { retain: true }
+    );
+    client.publish(
+      `${config.mqtt.topic}/${container.Id}/${currentTag}/update/attributes`,
+      JSON.stringify({
         current_version: previousDigest,
         new_version: newDigest,
-        update_available: true,
-        update_requested: false,
-      },
-    };
-
-    client.publish(
-      `${config.mqtt.topic}/${container.Id}/${currentTag}/state`,
-      JSON.stringify(update),
+        last_checked: new Date().toLocaleString(),
+      }),
       { retain: true }
     );
   }
 
-  static handleUpdate(
+  public static handleUpdate(
     container: Docker.ContainerInspectInfo,
     currentTag: string,
     newDigest: string,
-    client: mqtt.MqttClient
-  ): void {
-    const update = {
-      state: "updating",
-      attributes: {
-        friendly_name: `${container.Names[0].substring(1)}-${currentTag}`,
-        current_version: container.RepoDigests.find((d) => d.endsWith(`:${currentTag}`)),
-        new_version: newDigest,
-        update_available: false,
-        update_requested: true,
-      },
-    };
-
+    client: any
+  ) {
+    console.debug(`🔄 Updating container ${container.Name.substring(1)}:${currentTag}`);
     client.publish(
-      `${config.mqtt.topic}/${container.Id}/${currentTag}/state`,
-      JSON.stringify(update),
+      `${config.mqtt.topic}/${container.Id}/${currentTag}/update/state`,
+      "Updating...",
       { retain: true }
     );
-
-    DockerService.updateContainer(container, currentTag, newDigest).then(() => {
-      const updated = {
-        state: "updated",
-        attributes: {
-          friendly_name: `${container.Names[0].substring(1)}-${currentTag}`,
+    DockerService.updateContainer(container, currentTag).then(() => {
+      console.debug(`✅ Container ${container.Name.substring(1)}:${currentTag} updated`);
+      client.publish(
+        `${config.mqtt.topic}/${container.Id}/${currentTag}/update/state`,
+        "Up-to-date",
+        { retain: true }
+      );
+      client.publish(
+        `${config.mqtt.topic}/${container.Id}/${currentTag}/update/attributes`,
+        JSON.stringify({
           current_version: newDigest,
           new_version: newDigest,
-          update_available: false,
-          update_requested: false,
-        },
-      };
-
+          last_checked: new Date().toLocaleString(),
+        }),
+        { retain: true }
+      );
       client.publish(
         `${config.mqtt.topic}/${container.Id}/${currentTag}/state`,
-        JSON.stringify(updated),
+        "Up-to-date",
+        { retain: true }
+      );
+      client.publish(
+        `${config.mqtt.topic}/${container.Id}/${currentTag}/attributes`,
+        JSON.stringify({
+          current_version: newDigest,
+          last_checked: new Date().toLocaleString(),
+        }),
         { retain: true }
       );
     });
