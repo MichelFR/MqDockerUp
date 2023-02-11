6faf645da3ba23f94e8502177f2424bb4d2007a8
Michel R <michelfr95@gmail.com>
2023-02-11 14:24:13 +0100

    Add default values to ConfigService
    This commit adds default values to the ConfigService class. It also modifies the getConfig method to merge the config values with the default values if the values cannot be found in the config.yaml file or in the environment variables.

--- src/services/ConfigService.ts
+++ src/services/ConfigService.ts
@@ -4,13 +4,37 @@ import fs from "fs";
 export default class ConfigService {
   public static getConfig(): any {
     try {
+      // Define the default values
+      const defaults = {
+        main: {
+          interval: "15m",
+        },
+        mqtt: {
+          ha_discovery: true,
+          connectionUri: "mqtt://localhost:1883",
+          topic: "mqdockerup",
+          clientId: "mqdockerup",
+          username: "ha",
+          password: "12345678",
+          connectTimeout: 60,
+          protocolVersion: 5,
+          qos: 2,
+          retain: false,
+        },
+      };
+
+      // Parse the config file
       const config = yaml.parse(fs.readFileSync("config.yaml", "utf8"));
+
+      // Override the config values with the environment variables
       for (const key of Object.keys(config)) {
         if (process.env[key]) {
           config[key] = process.env[key];
         }
       }
-      return config;
+
+      // Merge the config values with the default values
+      return { ...defaults, ...config };
     } catch (e) {
       console.log(e);
     }
