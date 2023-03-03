0994e0bf4cada47e1f701cf8e29271f44cbb7728
Michel R <michelfr95@gmail.com>
2023-03-03 20:47:31 +0100

    Add new logging functionality, Add Winston, Add ASCII art

---
+++ src/services/LoggerService.ts
@@ -0,0 +1,72 @@
+import winston from 'winston';
+import figlet from 'figlet';
+import fs from 'fs';
+import path from 'path';
+
+const packageJson = require("../../../package.json");
+
+const logsDirectory = path.join(__dirname, 'logs');
+
+if (!fs.existsSync(logsDirectory)) {
+  fs.mkdirSync(logsDirectory);
+}
+
+const logFormat = winston.format.combine(
+  winston.format.colorize(),
+  winston.format.timestamp({ format: 'DD.MM.YYYY HH:mm:ss' }),
+  winston.format.align(),
+  winston.format.printf(info => {
+    const { timestamp, level, message, ...args } = info;
+
+    let output = `\x1b[90m${timestamp}\x1b[0m [${level}] ${message}`;
+
+    const extraData = Object.keys(args).length ? JSON.stringify(args, null, 2) : '';
+
+    if (extraData) {
+      output += `\n${extraData}`;
+    }
+
+    return output;
+  })
+);
+
+
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: logFormat,
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: path.join(logsDirectory, 'app.log'),
+      maxsize: 1024 * 1024 * 10, // 10 MB
+      maxFiles: 15,
+      tailable: true,
+      zippedArchive: true
+    }),
+    new winston.transports.File({
+      filename: path.join(logsDirectory, 'error.log'),
+      level: 'error',
+      maxsize: 1024 * 1024 * 10, // 10 MB
+      maxFiles: 15,
+      tailable: true,
+      zippedArchive: true
+    })
+  ]
+});
+
+const versionAscii = figlet.textSync(`MqDockerUp`, {
+  font: 'Standard',
+  horizontalLayout: 'default',
+  verticalLayout: 'default'
+});
+
+const clearConsole = () => {
+  console.clear();
+  console.log(`\n${versionAscii}\nStarting \x1b[36mMqDockerUp\x1b[0m V${packageJson.version} ... \n`);
+};
+
+clearConsole();
+
+export default logger;
+export { clearConsole };
