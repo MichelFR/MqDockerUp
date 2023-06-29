3421accfb8cc838274ba04abbcaf5e6103d9b9a1
Michel R <michelfr95@gmail.com>
2023-06-29 20:59:37 +0200

    Rewrite registry handling, add github registry support (#83)
    * Add initial adapter logic, tests, mocks
    
    * Add placeholder GithubAdapter
    
    * Update node_modules
    
    * Use RegistryAdapter, add GithubAdapter
    
    * Fix github auth
    
    * Potential fix for unified header auth
    
    * Potential fix the second
    
    * Add fail-save check for dockerhub image
    
    * fix merge conflict
    
    * RELEASE V 1.0.0
    
    * Fix missing github token
    
    * Return oldDigest when missing authToken
    
    * Return null when failed getting latest digest
    
    * Sanitize URL checks to prevent security issue
    
    * Rework canHandleImage to fix failed test
    
    * Fix package.json scan

---
+++ src/registry-factory/ImageRegistryAdapterFactory.ts
@@ -0,0 +1,30 @@
+import { DockerhubAdapter } from './DockerhubAdapter';
+import { GithubAdapter } from './GithubAdapter';
+import { ImageRegistryAdapter } from './ImageRegistryAdapter';
+
+export class ImageRegistryAdapterFactory {
+    private static registryAdapters = [
+        DockerhubAdapter,
+        GithubAdapter,
+        // add other adapters here in a similar way
+    ];
+
+    static getAdapter(image: string, tag?: string, accessToken?: string): ImageRegistryAdapter {
+        for (const adapter of this.registryAdapters) {
+            if (adapter.canHandleImage(image)) {
+                return new adapter(image, tag, accessToken);
+            }
+        }
+        throw new Error(`No adapter found for the image: ${image}`);
+    }
+
+    static getRegistryName(image: string): string {
+        for (const adapter of this.registryAdapters) {
+            if (adapter.canHandleImage(image)) {
+                return adapter.displayName;
+            }
+        }
+        throw new Error(`No registry found for the image: ${image}`);
+    }
+
+}
