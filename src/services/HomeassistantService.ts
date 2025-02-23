6a4a9348330443ab71ff46b00381c455ad8124f7
Elias Wernicke <e.w.1111111119@gmail.com>
2025-02-23 11:52:09 +0100

    add repo link back as fallback

--- src/services/HomeassistantService.ts
+++ src/services/HomeassistantService.ts
@@ -465,7 +465,7 @@ export default class HomeassistantService {
           installed_version: `${tag}: ${currentDigest?.substring(0, 12)}`,
           latest_version: newDigest ? `${tag}: ${newDigest?.substring(0, 12)}` : null,
           release_summary: "",
-          release_url: `${sourceRepo}`,
+          release_url: `${sourceRepo ? sourceRepo : "https://github.com/MichelFR/MqDockerUp"}`,
           entity_picture: "https://raw.githubusercontent.com/MichelFR/MqDockerUp/refs/heads/main/assets/logo_200x200.png",
           title: `${image}:${tag}`,
           in_progress: false,
